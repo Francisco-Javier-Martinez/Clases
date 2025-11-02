e5ad01faefd3584ea02d0b89aedbccbf06652984
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-11-02 14:16:24 +0100

    e

--- cliente/cliente/12-orientadosObjeto/juegoTrono/clases/personaje.js
+++ cliente/cliente/12-orientadosObjeto/juegoTrono/clases/personaje.js
@@ -1,3 +1,69 @@
+<<<<<<< HEAD
+import { Casa } from "./casa.js";
+import { Arma } from "./arma.js";
+export class Personaje{
+    #nombrePersonaje;
+    #edad;
+    #live;
+    #casaPerteneciente;
+    #arma;
+
+    constructor(){
+        this.#nombrePersonaje="";
+        this.#edad = 0;
+        this.#live = true;
+        this.#casaPerteneciente="";
+    }
+    //SET
+    set meterNombrePersonaje(nombre){
+        this.#nombrePersonaje=nombre;
+    }
+    set meterEdad(_edad){
+        this.#edad=_edad;
+    }
+    
+    meterCasa(cas){
+        if (!(cas instanceof Casa)) {
+            console.log("Error: Solo se pueden añadir instancias de Casa");
+            return;
+        }
+        this.#casaPerteneciente=cas;
+    }
+    meterArma(arm){
+        if (!(arm instanceof Arma)) {
+            console.log("Error: Solo se pueden añadir instancias de Arma");
+            return;
+        }
+        this.#arma=arm;
+    }
+    presentarse(){
+        let nombreCasa=this.#casaPerteneciente.getNombreCasa();
+        console.log("Soy "+this.#nombrePersonaje+" y tengo "+ this.#edad+" y pertenezco a la casa: "+nombreCasa);
+    }
+
+    // Getters
+    getNombrePersonaje(){
+        return this.#nombrePersonaje;
+    }
+
+    getEdad(){
+        return this.#edad;
+    }
+
+    estaVivo(){
+        return this.#live;
+    }
+
+    getCasaPerteneciente(){
+        return this.#casaPerteneciente;
+    }
+
+    getArma(){
+        return this.#arma;
+    }
+
+
+=======
 import { Casa } from "./casa.js";
 import { Arma } from "./arma.js";
 export class Personaje{
@@ -61,4 +127,5 @@ export class Personaje{
     }
 
 
+>>>>>>> f755361ee5e9c8cb9624ae4c46773d2fcfd6c9f8
 }
