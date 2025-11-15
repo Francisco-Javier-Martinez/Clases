a54a0837590ae72f929e30fe1a741484fe836caa
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-11-15 17:35:26 +0100

    header.php

---
+++ despliege-servidor/tareaServidoBaseDatosConHeaderr/borrar.php
@@ -0,0 +1,18 @@
+<?php
+    require_once 'boletin_Usuario.php';
+    $boletinUsuario= new Boletin_Usuario();
+    $usu=$_GET['idUsuario'];
+    $boletinUsuario->borrarUsuario($usu);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Configuracion</title>
+</head>
+<body>
+    
+</body>
+</html>
