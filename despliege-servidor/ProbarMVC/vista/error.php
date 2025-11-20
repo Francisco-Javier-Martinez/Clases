2279ea125c7da3f465b4b23084004e1e0284b18d
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-11-20 17:38:59 +0100

    s

--- despliege-servidor/ProbarMVC/vista/error.php
+++ despliege-servidor/ProbarMVC/vista/error.php
@@ -22,6 +22,6 @@
             }
         }
     ?>
-    <a href="<?php echo isset($enlace_volver) ? $enlace_volver : '../index.php'; ?>">Volver</a>
+    <a href="<?php echo $enlace_volver; ?>">Volver</a>
 </body>
 </html>
