60ad79ee839f15c323e4685f733692074a283b8d
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-08 12:00:04 +0100

    sdfghjkl

--- despliege-servidor/login/recibir.php
+++ despliege-servidor/login/recibir.php
@@ -2,12 +2,20 @@
     include_once 'usuario.php';
 
     $usuario=new Usuario();
-    $resultado=$usuario->comprobarUsuario();
+    if(isset($_POST['preparadas'])){
+        $resultado=$usuario->comprobarSeguro();
+    }else{
+        $resultado=$usuario->comprobarUsuario();
+    }
     $mensaje="";
     $mensaje1="";
     if($resultado!=false){
         $fila=$resultado->fetch_assoc();
         $mensaje= '<h1>Bienvenido usuario: '.$fila['nombre'].'</h1>';
+        if($fila['tipo']==0){
+            $mensaje1= '<h4>Eres administrador. Se te redirigirá al panel de administración
+            mira el contenido exclusivo para administradores</h4>';
+        }
     }else{
         $mensaje= '<h1>Usuario o contraseña incorrectos</h1>';
         $mensaje1= '<h4>Se te redirigirá al formulario de login en 3 segundos</h4>';
--- despliege-servidor/login/usuario.php
+++ despliege-servidor/login/usuario.php
@@ -19,6 +19,27 @@
                 echo '<h1>Error:'.$e->getMessage().'</h1>';
             }
         }
+
+        public function comprobarSeguro(){
+            try{
+                $correo=$_POST['correo'];
+                $contrasena=$_POST['contrasena'];
+
+                $sql="SELECT * from usuario where correo= ? && contraseña= ?;";
+                $stmt=$this->conexion->prepare($sql);
+                $stmt->bind_param("ss",$correo,$contrasena);
+                $stmt->execute();
+                $resultado=$stmt->get_result();
+                if($resultado->num_rows>0){
+                    return $resultado;
+                }else{
+                    return false;
+                }
+            }catch(mysqli_sql_exception $e){
+                echo '<h1>Error:'.$e->getCode().'</h1>'; 
+                echo '<h1>Error:'.$e->getMessage().'</h1>';
+            }
+        }
     }
 
 
