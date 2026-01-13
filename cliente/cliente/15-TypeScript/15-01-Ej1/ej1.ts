b4f21805865699406134f253fb671567c345a5b3
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-13 13:31:14 +0100

    j

---
+++ cliente/cliente/15-TypeScript/15-01-Ej1/ej1.ts
@@ -0,0 +1,21 @@
+//Ej1
+let nombre: string;
+nombre = "Miguelo";
+
+let edad: number;
+edad = 30;
+
+const PERSONAJE: { nombre: string; edad: number } = {
+  nombre: nombre,
+  edad: edad
+};
+//Ej2
+interface spiderman{
+    nombre: string;
+    poderes: string[];
+}
+
+const SPIDERMAN: spiderman ={
+    nombre:"Peter Parker",
+    poderes:["trepar","fuerza","agilidad","telas de araña"]
+};
---
+++ cliente/cliente/15-TypeScript/15-01-Ej1/rombo.ts
@@ -0,0 +1,10 @@
+class Rombo{
+    diagonalVertical: Number;
+    diagonalHorizontal: number;
+
+    constructor(diagonalVertical: number, diagonalHorizontal: number){
+        this.diagonalVertical = diagonalVertical;
+        this.diagonalHorizontal = diagonalHorizontal;
+    }
+    
+}
