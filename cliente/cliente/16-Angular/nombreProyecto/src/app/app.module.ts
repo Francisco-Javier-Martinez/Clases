5eb719e33650d55c0a3cd8346baa307aa560a263
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-15 13:21:47 +0100

    erty

---
+++ cliente/cliente/16-Angular/nombreProyecto/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentePruebaComponent } from './components/componente-prueba/componente-prueba.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentePruebaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
