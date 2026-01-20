d8727e4d720eaa34cd42c3295add7362f01cc456
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-20 13:16:59 +0100

    tarea alberto

--- cliente/cliente/16-Angular/tiendaProductos/src/app/componentes/componente-productos/componente-productos.component.ts
+++ cliente/cliente/16-Angular/tiendaProductos/src/app/componentes/componente-productos/componente-productos.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ServicioProductosService } from '../../servicios/servicio-productos.service';
 @Component({
   selector: 'app-componente-productos',
   templateUrl: './componente-productos.component.html',
   styleUrls: ['./componente-productos.component.css']
 })
 export class ComponenteProductosComponent {
+  //creamos un constructor donde instanciamos el servicio 
+  constructor(private servicioProductos: ServicioProductosService) { }
+
+  //recogerProductos
+  productos: any[] = []; //array vacio
+
+  //Este metodo inicializa la vida del componente
+  ngOnInit() {
+    this.productos = this.servicioProductos.getProductos();
+  }
 
 }
--- cliente/cliente/16-Angular/tiendaProductos/src/app/servicios/servicio-productos.service.ts
+++ cliente/cliente/16-Angular/tiendaProductos/src/app/servicios/servicio-productos.service.ts
@@ -7,11 +7,16 @@ export class ServicioProductosService {
 
   constructor() {}
 
+  //array
   private productos =[
-    {id: 1, nombre: 'Producto 1', precio: 100},
-    {id: 2, nombre: 'Producto 2', precio: 200},
-    {id: 3, nombre: 'Producto 3', precio: 300}
+    {id: 1, nombre: 'Maceta', precio: 1300},
+    {id: 2, nombre: 'Papel de baño', precio: 2040},
+    {id: 3, nombre: 'Mando de Play 10', precio: 67}
   ];
 
-  
+  //getProductos
+  getProductos(){
+    return this.productos;
+  }
+   
 }
