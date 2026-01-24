3465337cbfc613340ee5af922d82188ac90eb207
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-24 17:22:43 +0100

    sda

--- cliente/cliente/16-Angular/carnavalProject2/src/app/directivas/d-comparsas.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DComparsasDirective } from './d-comparsas.directive';
-
-describe('DComparsasDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DComparsasDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- cliente/cliente/16-Angular/carnavalProject2/src/app/pipes/p-comparsas.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PComparsasPipe } from './p-comparsas.pipe';
-
-describe('PComparsasPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PComparsasPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
