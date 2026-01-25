c0e547fbd9f798b5966575a5a188c580807cb4e5
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-25 13:34:02 +0100

    v

---
+++ cliente/cliente/16-Angular/albertoBien/src/app/componentes/profesores/profesores.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Profesore } from '../../servicios/profesore';
+
+@Component({
+  selector: 'app-profesores',
+  standalone: false,
+  templateUrl: './profesores.html',
+  styleUrl: './profesores.css',
+})
+export class Profesores {
+constructor(private objProfe: Profesore){};
+  //variables
+  profes: any[] =[];
+  nuevoPorfeNombre: string= '';
+  nuevoProfeAsigntura:string='';
+
+  //inicializar los profes
+  ngOnInit(){
+    this.profes=this.objProfe.getProfesores();
+  }
+
+  borrarProfe(id:number){
+    this.objProfe.borrarProfe(id);
+  }
+
+  actualzarProfe(id:number){
+    this.objProfe.editar(id);
+  }
+}
--- cliente/cliente/16-Angular/albertoBien/src/app/servicios/profesore.ts
+++ cliente/cliente/16-Angular/albertoBien/src/app/servicios/profesore.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Profesores {
-  
+export class Profesore {
   arrayProfes:{id:number,nombre:string,asignatura:string}[]=[
     {id:1,nombre:'Alberto',asignatura:'Angular'},
     {id:2,nombre:'María',asignatura:'React'},
@@ -22,18 +21,19 @@ export class Profesores {
   }
 
   borrarProfe(idBorrar:number){
-    this.arrayProfes.splice(idBorrar,1);
+    this.arrayProfes.forEach((element) => {
+      if(element.id==idBorrar){
+        let indiceBorrar=this.arrayProfes.indexOf(element);
+        this.arrayProfes.splice(indiceBorrar,1);
+      }
+    });
   }
 
   editar(idEdtiar:number){
     let nuevoNom= prompt("Mete nuevo nombre");
-    if(nuevoNom && nuevoNom.length<3){
-      alert("El nombre debe tener al menos 3 caracteres");
-      return;
-    }
     let nuevaAsig= prompt("Mete nueva asignatura");
-    if(nuevaAsig && nuevaAsig.length<3){
-      alert("La asignatura debe tener al menos 3 caracteres");
+    if(!nuevoNom || !nuevaAsig){
+      alert("Valor no valido");
       return;
     }
     this.arrayProfes.forEach(element => {
