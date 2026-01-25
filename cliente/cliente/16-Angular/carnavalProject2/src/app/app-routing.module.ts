85f7a36efc55e098ae0d3dffe2b3057237a302a5
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2026-01-25 12:05:49 +0100

    terminado

--- cliente/cliente/16-Angular/carnavalProject2/src/app/app-routing.module.ts
+++ cliente/cliente/16-Angular/carnavalProject2/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComparsaListComponent } from './componentes/comparsa-list/comparsa-list.component';
+import { JuradoComponent } from './componentes/jurado/jurado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'comparsas',component:ComparsaListComponent},
+  {path:'jurado',component:JuradoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
